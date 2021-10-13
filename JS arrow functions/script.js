cc3ab1c366a85a72c834fd94728173f1f38588be
Roisin-13 <roisin.wheeler@gmail.com>
2021-10-13 10:13:02 +0100

    arrow functions explored

---
+++ JS arrow functions/script.js
@@ -0,0 +1,12 @@
+subtract = (num1, num2) => console.log(num1 - num2);
+subtract(52, 12);
+
+function welcome(name, age, gender){
+    return console.log(`My name is ${name}, i am ${age} years old and of gender ${gender}`)
+}
+welcome("Felix Cited", 20, "Male");
+
+
+powerUp = (n1, n2) => console.log(Math.pow(n1, n2));
+powerUp(2, 3);
+powerUp(3, 3);
